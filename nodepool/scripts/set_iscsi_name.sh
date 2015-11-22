fbf2cef3ed5498ec97b947e3df2f608d0106ed35
Siddharth Bhatt <siddharth.bhatt@falconstor.com>
2015-11-22 13:37:20 -0500

    Set iSCSI initiator name based on unique node name

--- nodepool/scripts/set_iscsi_name.sh
+++ nodepool/scripts/set_iscsi_name.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -xe
 
+# Nodepool invokes this script and passes the node name as a parameter
+# The node name contains the node ID as a suffix, so we know it is unique
+
 HOSTNAME=$1
 
+# Set hostname
 sudo hostname $HOSTNAME
 if [ -n "$HOSTNAME" ] && ! grep -q $HOSTNAME /etc/hosts ; then
     echo "127.0.1.1 $HOSTNAME" | sudo tee -a /etc/hosts
 fi
-
 echo $HOSTNAME > /tmp/image-hostname.txt
 sudo mv /tmp/image-hostname.txt /etc/image-hostname.txt
 
-INITNAME="InitiatorName=iqn.1993-08.org.debian:01:$RANDOM"
+# Set iSCSI initiator name to make it unique
+INITNAME="InitiatorName=iqn.1993-08.org.debian:01:$HOSTNAME"
 echo "$INITNAME" | sudo tee /etc/iscsi/initiatorname.iscsi
