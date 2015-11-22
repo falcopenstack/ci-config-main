ed1ffa162a2191480f5d6c603a1ca10f6f412d77
Siddharth Bhatt <siddharth.bhatt@falconstor.com>
2015-11-22 12:13:59 -0500

    Set hostname during ready-script

--- nodepool/scripts/set_iscsi_name.sh
+++ nodepool/scripts/set_iscsi_name.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -xe
 
+HOSTNAME=$1
+
+sudo hostname $HOSTNAME
+if [ -n "$HOSTNAME" ] && ! grep -q $HOSTNAME /etc/hosts ; then
+    echo "127.0.1.1 $HOSTNAME" | sudo tee -a /etc/hosts
+fi
+
+echo $HOSTNAME > /tmp/image-hostname.txt
+sudo mv /tmp/image-hostname.txt /etc/image-hostname.txt
+
 INITNAME="InitiatorName=iqn.1993-08.org.debian:01:$RANDOM"
-sudo echo "$INITNAME" > /etc/iscsi/initiatorname.iscsi
+echo "$INITNAME" | sudo tee /etc/iscsi/initiatorname.iscsi
