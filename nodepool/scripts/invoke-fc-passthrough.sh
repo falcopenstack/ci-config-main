0984d5c048a7eff0806d90dded3996c49162221c
Chong Kim <chong.kim@falconstor.com>
2016-07-06 11:27:28 -0400

    Update to set hostname first

--- nodepool/scripts/invoke-fc-passthrough.sh
+++ nodepool/scripts/invoke-fc-passthrough.sh
@@ -34,6 +34,31 @@
 # export FC_PROVIDER_USER=root
 # export FC_PROVIDER_KEY=/opt/nodepool-scripts/passthrough
 # export FC_PROVIDER_RC=/root/keystonerc_jenkins
+
+HOSTNAME=$1
+
+# Set hostname
+sudo hostname $HOSTNAME
+if [ -n "$HOSTNAME" ] && ! grep -q $HOSTNAME /etc/hosts ; then
+    echo "127.0.1.1 $HOSTNAME" | sudo tee -a /etc/hosts
+fi
+echo $HOSTNAME > /tmp/image-hostname.txt
+sudo mv /tmp/image-hostname.txt /etc/image-hostname.txt
+
+# Quick fix so that slave node can resolve the master
+echo "192.168.2.32 ci-master" | sudo tee -a /etc/hosts
+
+# Set iSCSI initiator name to make it unique
+INITNAME=`sudo iscsi-iname`
+if [ -z "$INITNAME" ]; then
+    INITNAME="iqn.1993-08.org.debian:01:$HOSTNAME"
+fi
+echo "InitiatorName=$INITNAME" | sudo tee /etc/iscsi/initiatorname.iscsi
+sudo service open-iscsi restart
+
+#
+# FC Passthrough setup starts here
+#
 echo "Export FC_PROVIDER env variable"
  export FC_PROVIDER=192.168.2.70
  export FC_PROVIDER_USER=root
